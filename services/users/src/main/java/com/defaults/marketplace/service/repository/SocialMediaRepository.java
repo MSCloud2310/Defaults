3afad513bdd0a637aabd1c7bbc9a3876eedf2821
David Ramírez <ramirezmonroyd@gmail.com>
2023-03-23 02:02:31 -0500

    Delete social media repository due to uselessness

--- services/users/src/main/java/com/defaults/marketplace/service/repository/SocialMediaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.defaults.marketplace.service.repository;
-
-import com.defaults.marketplace.service.model.SocialMedia;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SocialMediaRepository extends CrudRepository<SocialMedia, Integer> {
-}
