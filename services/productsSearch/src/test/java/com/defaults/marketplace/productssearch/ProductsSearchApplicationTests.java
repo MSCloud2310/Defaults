65cedac4fa503f2bc7ee015c332050ed49a3ccbc
Angie Tatiana Peña <penap_at@javeriana.edu.co>
2023-03-26 18:04:02 -0500

    create base project

---
+++ services/productsSearch/src/test/java/com/defaults/marketplace/productssearch/ProductsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defaults.marketplace.productssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
