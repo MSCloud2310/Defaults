b67871f642633b8b9c008670a4a94f4b865bc5f7
Jessica Naizaque <jessicanaizaque1@gmail.com>
2023-03-27 14:20:03 -0500

    Marketplace Server commit

---
+++ marketplace-server/src/main/java/com/defaults/marketplace/marketplaceserver/models/orders/PaymentDetails.java
@@ -0,0 +1,31 @@
+package com.defaults.marketplace.marketplaceserver.models.orders;
+
+public class PaymentDetails {
+
+    private PaymentMethod paymentMethod;
+    private String cardLastFourDigits;
+
+    public PaymentDetails(PaymentMethod paymentMethod, String cardLastFourDigits) {
+        this.paymentMethod = paymentMethod;
+        this.cardLastFourDigits = cardLastFourDigits;
+    }
+
+    public PaymentDetails() {
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getCardLastFourDigits() {
+        return cardLastFourDigits;
+    }
+
+    public void setCardLastFourDigits(String cardLastFourDigits) {
+        this.cardLastFourDigits = cardLastFourDigits;
+    }
+}
---
+++ marketplace-server/src/main/java/com/defaults/marketplace/marketplaceserver/models/orders/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.defaults.marketplace.marketplaceserver.models.orders;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD
+}
