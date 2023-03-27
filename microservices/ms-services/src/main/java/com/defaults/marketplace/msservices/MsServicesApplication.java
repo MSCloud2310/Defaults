dcbfe99adb8f4bf6dc859fb29de713ae42edc29f
Alejandro Morales <a.moralesc@javeriana.edu.co>
2023-03-27 04:04:24 -0500

    Enable eureka discovery client

--- microservices/ms-services/src/main/java/com/defaults/marketplace/msservices/MsServicesApplication.java
+++ microservices/ms-services/src/main/java/com/defaults/marketplace/msservices/MsServicesApplication.java
@@ -2,8 +2,10 @@ package com.defaults.marketplace.msservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsServicesApplication {
 
 	public static void main(String[] args) {
