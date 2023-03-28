d1fa0c081aa55f5276b1604e3f2ceab584c4334b
David Ramirez <ramirezmonroyd@gmail.com>
2023-03-28 13:10:00 -0500

    Delete services folder
    It's neccesary to move this folder content into the microservices one

--- services/productSearch/src/main/java/com/defaults/marketplace/productsearch/repository/ProviderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.defaults.marketplace.productsearch.repository;
-
-import com.defaults.marketplace.productsearch.models.entities.Provider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProviderRepository extends JpaRepository<Provider, Integer> {
-
-    Provider findProviderById(Integer id);
-}
