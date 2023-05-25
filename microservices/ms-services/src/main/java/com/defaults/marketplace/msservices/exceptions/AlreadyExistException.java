6c5ef841af348357b50909b0f402c0ff127eba4a
Angie Tatiana Peña <78171013+AngieTatianaP@users.noreply.github.com>
2023-05-25 13:22:12 -0500

    Revert "Feature/soap"

--- microservices/ms-services/src/main/java/com/defaults/marketplace/msservices/exceptions/AlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.defaults.marketplace.msservices.exceptions;
-
-public class AlreadyExistException extends RuntimeException{
-    public AlreadyExistException(String message){
-        super(message);
-    }
-}
--- microservices/ms-users/src/main/java/com/defaults/marketplace/msusers/models/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.defaults.marketplace.msusers.models;
-
-public enum UserRole {
-    CLIENT,
-    PROVIDER;
-}
